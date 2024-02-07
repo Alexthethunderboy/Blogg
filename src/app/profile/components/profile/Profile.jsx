8e1023831f97cbecdc4fe6a37fc0e395609c67c2
joehanzsenna <jonathanedison34@gmail.com>
2024-02-07 20:29:23 +0100

    feat: updated code

--- src/app/profile/components/profile/Profile.jsx
+++ src/app/profile/components/profile/Profile.jsx
@@ -11,32 +11,31 @@ import { profileData } from "./ProfileDb";
 
 export default function Profile() {
   return (
-    <div className="md:w-full md:max-w-[1440px]">
-      <div className="w-full h-[50vh] flex md:flex-row flex-col md:static items-center md:px-[6rem]  mb-16">
-        <div>
-          <Image className="md:w-[165px] md:mr-[30px] ml-[0] md:ml-[0] pt-10 md:pt-0" src={ellipse} alt="profile-image"/>
+    <div className="w-full md:max-w-[1440px]">
+      <div className="w-[85%] mx-auto h-[auto] flex md:flex-row flex-col items-center py-10 gap-x-6">
+        <div className="w-[4rem] md:w-[228px] ">
+          <Image className="w-full" src={ellipse} alt="profile-image"/>
         </div>
-
-        <div className="px- md:px-0">
-          <h1 className="font-bold text-md text-center md:text-left pt-4 md:pt-0 mb-3">Jane Doe</h1>
-          <p className="md:w-[700px]  text-center md:text-start md:ml-0 ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, dolore!
-            Qui ratione fuga distinctio facere ipsum natus praesentium rerum
-            laboriosam dolor consequatur, adipisci nisi perspiciatis nemo
-            cupiditate mollitia voluptatum beatae veniam consequuntur expedita
-            maiores quas.
-          </p>
-          <div className="mt-2 flex gap-3 pt-5 md:pt-0">
-            <span className="text-[12px]">15,000 views</span>
-            <span className="text-[12px]">60 Published</span>
+        <div className="w-full flex flex-col md:flex-row items-baseline">
+          <div className="w-full">
+            <h1 className="font-semibold text-3xl text-center md:text-left pt-4 md:pt-0 mb-3">Jane Doe</h1>
+            <p className="w-full md:w-[607px]  text-center md:text-start md:ml-0 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, dolore!
+              Qui ratione fuga distinctio facere ipsum natus praesentium rerum
+              laboriosam dolor consequatur, adipisci nisi perspiciatis nemo
+              cupiditate mollitia voluptatum beatae veniam consequuntur expedita
+              maiores quas.
+            </p>
+            <div className="mt-2 flex gap-3 pt-5 md:pt-0">
+              <span className="text-[12px]">15,000 views</span>
+              <span className="text-[12px]">60 Published</span>
+            </div>
           </div>
-        </div>
-        <div className="right-40px bottom-[40px] md:static">
-          <button className="md:mx-auto md:ml-[140px] ml-[650px] border border-[2px #26BDD2] px-3 rounded-md">Edit</button>
+          <button className="md:mx-auto border border-[2px #26BDD2] px-3 rounded-md mt-5 md:mt-0">Edit</button>
         </div>
       </div>
 
-      <div className="w-[85%] mx-auto mt-10 mb-8  bg-red-500">
+      <div className="w-[85%] mx-auto mt-5 mb-8 ">
         <div className={styles.container}>
           <button className={styles.btn}>Published</button>
           <h3 className={styles.text}>Drafts</h3>
