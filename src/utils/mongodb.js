91baf0724907f43aa045fbf3445a737790618771
joehanzsenna <jonathanedison34@gmail.com>
2024-02-21 11:09:23 -0500

    updated

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -8,6 +8,7 @@ const connect = async () => {
     //     }
     try{
         await mongoose.connect(process.env.MONGO_URL)
+        console.log("Connected to MongoDB.");
     }catch(error){
         throw new Error("Error connecting to Mongoose")
     }
