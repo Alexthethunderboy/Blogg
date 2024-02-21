7b14b93d15b8903bfb603a5378c6e20e40cd2fcc
Alexthethunderboy <khaylex2.0@gmail.com>
2024-02-21 21:04:08 +0100

    latest commit

---
+++ src/Utils/mongodb.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+
+const connect = async () => {
+    if (mongoose.connections[0].readyState) return;
+    //  {
+    //         useNewUrlParser: true,
+    //         useUnifiedTopology: true,
+    //     }
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("Connected to MongoDB.");
+    }catch(error){
+        throw new Error("Error connecting to Mongoose")
+    }
+}
+
+export default connect
